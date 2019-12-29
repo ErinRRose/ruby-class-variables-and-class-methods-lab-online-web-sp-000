88213892b9aa46d25fb89227eed68c711b6d08be
Erin  Rose <errose10@gmail.com>
2019-12-29 16:45:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
+  @@artist
 
 
   def initialize(name, artist, genre)
@@ -9,6 +10,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count +=1
+    @@artist
   end
 
   def self.count
